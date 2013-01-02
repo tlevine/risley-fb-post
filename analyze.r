8a49b23ec8a983afe4985cb206c7e000c5bccf38
Thomas Levine <occurrence@thomaslevine.com>
2013-01-02 17:37:51 -0500

    plots

--- analyze.r
+++ analyze.r
@@ -1,9 +1,11 @@
 #!/usr/bin/env Rscript
-
-times <- read.csv('time.csv', header=F)[,1]
-times.gmt <- strptime(times, format = '%d/%b/%Y:%H:%M:%S %z')
-times.est <- strptime(times, format = '%d/%b/%Y:%H:%M:%S -0500')
-
 library(ggplot2)
-qplot(times.gmt)
 
+chainsaw <- read.csv('time.csv')
+chainsaw$datetime.gmt <- strptime(chainsaw$datetime, format = '%d/%b/%Y:%H:%M:%S %z')
+chainsaw$datetime.est <- strptime(chainsaw$datetime, format = '%d/%b/%Y:%H:%M:%S -0500')
+
+p.1 <- qplot(chainsaw$datetime.est)
+p.2 <- qplot(chainsaw$request)
+p.3 <- ggplot(subset(chainsaw, request %in% names(sort(table(chainsaw$request), decreasing = T)[1:20]))) +
+  aes(x = datetime.est, y = request) + geom_bin2d()
